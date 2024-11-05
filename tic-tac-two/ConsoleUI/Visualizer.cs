1cdab9b8f76e0f621cae7bd98928ee47993ede62
anrokk <anrokk@taltech.ee>
2024-11-06 00:39:19 +0200

    Added specific colours to grid and game pieces, changed some logic.

--- tic-tac-two/ConsoleUI/Visualizer.cs
+++ tic-tac-two/ConsoleUI/Visualizer.cs
@@ -4,13 +4,51 @@ namespace ConsoleUI;
 
 public static class Visualizer
 {
+    private const string GridBackgroundColor = "\u001b[47m";
+    
+    private const string PieceOColor = "\u001b[34m";
+    
+    private const string PieceXColor = "\u001b[31m";
+    
+    private const string ResetToNormal = "\u001b[0m";
+    
     public static void DrawBoard(TicTacTwoBrain gameInstance)
     {
+        var gridStartX = gameInstance.GridStartX;
+        var gridStartY = gameInstance.GridStartY;
+        var gridEndX = gameInstance.GridEndX;
+        var gridEndY = gameInstance.GridEndY;
+        
+        Console.Write("   ");
+        for (var x = 0; x < gameInstance.DimensionX; x++)
+        {
+            Console.Write($" {x}  ");
+        }
+        Console.WriteLine();
+        
         for (var y = 0; y < gameInstance.DimensionY; y++)
         {
+            Console.Write($"{y}  ");
+            
             for (var x = 0; x < gameInstance.DimensionX; x++)
             {
-                Console.Write(" " + DrawGamePiece(gameInstance.GameBoard[x][y]) + " ");
+
+                var moveInGrid = x >= gridStartX && x <= gridEndX && y >= gridStartY && y <= gridEndY;
+
+                if (moveInGrid)
+                {
+                    Console.Write(GridBackgroundColor +
+                                  " " +
+                                  DrawGamePiece(gameInstance.GameBoard[x][y]) +
+                                  " " +
+                                  ResetToNormal
+                    );
+                }
+                
+                else
+                {
+                    Console.Write(" " + DrawGamePiece(gameInstance.GameBoard[x][y]) + " ");
+                }
                 
                 if (x != gameInstance.DimensionX - 1)
                 {
@@ -22,6 +60,7 @@ public static class Visualizer
             
             if (y != gameInstance.DimensionY - 1)
             {
+                Console.Write("   ");
                 for (var x = 0; x < gameInstance.DimensionX; x++)
                 {
                     Console.Write("---");
@@ -34,13 +73,12 @@ public static class Visualizer
             }
         }
     }
-    
     private static string DrawGamePiece(EGamePiece piece)
     {
         return piece switch
         {
-            EGamePiece.O => "O",
-            EGamePiece.X => "X",
+            EGamePiece.O => PieceOColor + "O" + ResetToNormal,
+            EGamePiece.X => PieceXColor + "X" + ResetToNormal,
             _ => " "
         };
     }
