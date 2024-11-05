252e197d1274abfd1faaabed6c0651016df3aceb
anrokk <anrokk@taltech.ee>
2024-11-05 16:34:03 +0200

    Grid movement optimized

--- tic-tac-two/GameLogic/GameState.cs
+++ tic-tac-two/GameLogic/GameState.cs
@@ -5,6 +5,10 @@ public class GameState
     public EGamePiece[][] GameBoard { get; set; }
     public EGamePiece NextMoveBy { get; set; } = EGamePiece.X;
     public GameConfiguration GameConfiguration { get; set; }
+    public int GridStartX { get; set; }
+    public int GridStartY { get; set; }
+    public int GridEndX { get; set; }
+    public int GridEndY { get; set; }
     
     public GameState(EGamePiece[][] gameBoard, GameConfiguration gameConfiguration)
     {
--- tic-tac-two/GameLogic/TicTacTwoBrain.cs
+++ tic-tac-two/GameLogic/TicTacTwoBrain.cs
@@ -12,6 +12,13 @@ public class TicTacTwoBrain
             gameBoard[x] = new EGamePiece[gameConfiguration.BoardSizeHeight];
         }
         _gameState = new GameState(gameBoard, gameConfiguration);
+        
+        CenterGrid();
+    }
+
+    public TicTacTwoBrain(GameState gameState)
+    {
+        _gameState = gameState;
     }
 
     public string GetGameStateJson()
@@ -29,14 +36,43 @@ public class TicTacTwoBrain
         get => GetBoard();
         private set => _gameState.GameBoard = value;
     }
+
+    public int GridStartX
+    {
+        get => _gameState.GridStartX;
+        private set => _gameState.GridStartX = value;
+    }
+
+    public int GridStartY
+    {
+        get => _gameState.GridStartY;
+        private set => _gameState.GridStartY = value;
+    }
+
+    public int GridEndX
+    {
+        get => _gameState.GridEndX;
+        private set => _gameState.GridEndX = value;
+    }
+
+    public int GridEndY
+    {
+        get => _gameState.GridEndY;
+        private set => _gameState.GridEndY = value;
+    }
     
     public int DimensionX => _gameState.GameBoard.Length;
     public int DimensionY => _gameState.GameBoard.Length > 0 ? _gameState.GameBoard[0].Length : 0;
-
-
+    
     private void CenterGrid()
     {
-        //TODO
+        var gridWidth = _gameState.GameConfiguration.GridSizeWidth;
+        var gridHeight = _gameState.GameConfiguration.GridSizeHeight;
+
+        _gameState.GridStartX = (DimensionX - gridWidth) / 2;
+        _gameState.GridStartY = (DimensionY - gridHeight) / 2;
+        _gameState.GridEndX = _gameState.GridStartX + gridWidth - 1;
+        _gameState.GridEndY = _gameState.GridStartY + gridHeight - 1;
     }
     
     private EGamePiece[][] GetBoard()
@@ -56,33 +92,126 @@ public class TicTacTwoBrain
 
     public bool MakeAMove(int x, int y)
     {
-        //TO IMPROVE
+        if (x < _gameState.GridStartX || x > _gameState.GridEndX || 
+            y < _gameState.GridStartY || y > _gameState.GridEndY)
+        {
+            Console.WriteLine("Move is outside of the grid.");
+        }
+
         if (_gameState.GameBoard[x][y] != EGamePiece.Empty)
         {
+            Console.WriteLine("This square is already occupied.");
             return false;
         }
+        
         _gameState.GameBoard[x][y] = _gameState.NextMoveBy;
         _gameState.NextMoveBy = _gameState.NextMoveBy == EGamePiece.X ? EGamePiece.O : EGamePiece.X;
+        
         return true;
     }
 
     public bool MoveGrid(string direction)
     {
-        //TODO
-        return true;
+        switch (direction.ToLower())
+        {
+            case "up":
+                if (_gameState.GridStartY > 0)
+                {
+                    _gameState.GridStartY--;
+                    _gameState.GridEndY--;
+                    return true;
+                }
+                break;
+            
+            case "down":
+                if (_gameState.GridEndY < DimensionY - 1)
+                {
+                    _gameState.GridStartY++;
+                    _gameState.GridEndY++;
+                    return true;
+                }
+                break;
+            
+            case "left":
+                if (_gameState.GridStartX > 0)
+                {
+                    _gameState.GridStartX--;
+                    _gameState.GridEndX--;
+                    return true;
+                }
+                break;
+            
+            case "right":
+                if (_gameState.GridEndX < DimensionX - 1)
+                {
+                    _gameState.GridStartX++;
+                    _gameState.GridEndX++;
+                    return true;
+                    
+                }
+                break;
+            
+            case "up-left":
+                if (_gameState.GridStartY > 0 && _gameState.GridStartX > 0)
+                {
+                    _gameState.GridStartY--;
+                    _gameState.GridEndY--;
+                    _gameState.GridStartX--;
+                    _gameState.GridEndX--;
+                    return true;
+                }
+                break;
+            
+            case "up-right":
+                if (_gameState.GridStartY > 0 && _gameState.GridEndX < DimensionX - 1)
+                {
+                    _gameState.GridStartY--;
+                    _gameState.GridEndY--;
+                    _gameState.GridStartX++;
+                    _gameState.GridEndX++;
+                    return true;
+                }
+                break;
+            case "down-left":
+                if (_gameState.GridEndY < DimensionY - 1 && _gameState.GridStartX > 0)
+                {
+                    _gameState.GridStartY++;
+                    _gameState.GridEndY++;
+                    _gameState.GridStartX--;
+                    _gameState.GridEndX--;
+                    return true;
+                }
+                break;
+
+            case "down-right":
+                if (_gameState.GridEndY < DimensionY - 1 && _gameState.GridEndX < DimensionX - 1)
+                {
+                    _gameState.GridStartY++;
+                    _gameState.GridEndY++;
+                    _gameState.GridStartX++;
+                    _gameState.GridEndX++;
+                    return true;
+                }
+                break;
+            
+            default:
+                Console.WriteLine("");
+                break;
+        }
+        return false;
     }
 
     public void ResetGame()
     {
         var gameBoard = new EGamePiece[_gameState.GameConfiguration.BoardSizeWidth][];
+        
         for (var x = 0; x < gameBoard.Length; x++)
         {
             gameBoard[x] = new EGamePiece[_gameState.GameConfiguration.BoardSizeHeight];
         }
+        
         _gameState.GameBoard = gameBoard;
         _gameState.NextMoveBy = EGamePiece.X;
-        
-        //TO REFERENCE CENTER GRID
-
+        CenterGrid();
     }
 }
